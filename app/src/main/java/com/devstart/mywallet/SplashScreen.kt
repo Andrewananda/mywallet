9900a22c89179e2123194b4859e1002ae712d54c
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-08-12 15:40:22 +0300

    user checker

--- app/src/main/java/com/devstart/mywallet/SplashScreen.kt
+++ app/src/main/java/com/devstart/mywallet/SplashScreen.kt
@@ -1,10 +1,13 @@
 package com.devstart.mywallet
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import androidx.navigation.findNavController
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +20,19 @@ class SplashScreen : AppCompatActivity() {
         )
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            if(userExist()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else {
+
+                finish()
+            }
         }, 3000)
+    }
+
+    private fun userExist(): Boolean {
+        val sharedPreferences = this.getSharedPreferences("user", Context.MODE_PRIVATE)
+        return sharedPreferences !== null
     }
 }
