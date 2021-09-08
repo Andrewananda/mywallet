8536da9865f80a922fb3d61b4348146f540e257d
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-09-08 07:57:01 +0300

    fixes

--- app/src/main/java/com/devstart/mywallet/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/devstart/mywallet/dashboard/DashboardFragment.kt
@@ -1,21 +1,35 @@
 package com.devstart.mywallet.dashboard
 
+import android.app.ActionBar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.devstart.mywallet.R
+import com.devstart.mywallet.databinding.FragmentDashboardBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DashboardFragment : Fragment() {
+
+    private lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        binding = FragmentDashboardBinding.inflate(layoutInflater, container, false)
+//        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.toolbar)
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
