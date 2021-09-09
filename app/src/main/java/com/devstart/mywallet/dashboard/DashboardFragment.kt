d66af4a87844230f9d693f676938ac480867ae2e
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-09-09 10:20:16 +0300

    fixes

--- app/src/main/java/com/devstart/mywallet/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/devstart/mywallet/dashboard/DashboardFragment.kt
@@ -1,13 +1,10 @@
 package com.devstart.mywallet.dashboard
 
-import android.app.ActionBar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import com.devstart.mywallet.R
 import com.devstart.mywallet.databinding.FragmentDashboardBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,6 +20,10 @@ class DashboardFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentDashboardBinding.inflate(layoutInflater, container, false)
 
+        binding.btnShowBalance.setOnClickListener {
+            binding.txtBalance.visibility = View.VISIBLE
+            binding.btnShowBalance.visibility = View.GONE
+        }
 
         return binding.root
     }
