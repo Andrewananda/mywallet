06d2b5909cc72866950cd95480ed2e14affbde93
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-09-09 22:05:22 +0300

    expenditure fragment

--- app/src/main/java/com/devstart/mywallet/expenditure/ui/ExpenditureFragment.kt
+++ app/src/main/java/com/devstart/mywallet/expenditure/ui/ExpenditureFragment.kt
@@ -1,16 +1,30 @@
 package com.devstart.mywallet.expenditure.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.Observer
 import com.devstart.mywallet.R
+import com.devstart.mywallet.data.Failure
+import com.devstart.mywallet.data.Success
 import com.devstart.mywallet.databinding.FragmentExpenditureBinding
+import com.devstart.mywallet.expenditure.viewModel.ExpenditureViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ExpenditureFragment : Fragment() {
 
     private lateinit var binding: FragmentExpenditureBinding
+
+    @Inject
+    lateinit var viewModel: ExpenditureViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +33,46 @@ class ExpenditureFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentExpenditureBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSubmit.setOnClickListener {
+            validateInputs()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun validateInputs() {
+        val amount = binding.txtAmount.text?.trim().toString()
+        val description = binding.txtDescription.text?.trim().toString()
+
+        when {
+            amount.isBlank() -> {
+                binding.amountLayout.error = "Amount field required"
+            }
+            description.isBlank() -> {
+                binding.descriptionLayout.error = "Description field required"
+            }
+            else -> {
+                viewModel.submitExpenditure(description, amount.toInt())
+                observeSubmitResponse()
+            }
+        }
+    }
+
+
+    private fun observeSubmitResponse() {
+        viewModel.expenditureResponse().observe(viewLifecycleOwner, Observer {
+            when(it) {
+                is Success<*> -> {
+                    Toast.makeText(context, "Added successfully", Toast.LENGTH_LONG).show()
+                }
+                is Failure -> {
+                    Log.i("ERROR", it.throwable.toString())
+                }
+            }
+        })
     }
 }
